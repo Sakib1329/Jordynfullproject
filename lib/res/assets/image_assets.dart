5792b28dd9892c65fb1edf56972b73987dcf5760
Sakib Tousib <sakibtousib1329@gmail.com>
2025-05-05 17:52:14 +0600

    Mahbub bhai er jonno abr directory

---
+++ lib/res/assets/image_assets.dart
@@ -0,0 +1,20 @@
+class ImageAssets {
+
+  //Icons
+
+  static const String splashScreen = 'assets/svg/splash.svg';
+
+
+   // home*
+
+  //onboard*
+
+
+
+
+
+
+  //PNG
+  static const String splashScreen1 = 'assets/images/splash.png';
+
+}
