e49ee6b226ff27b23a322f71ebffc5dd27f2204d
mahbubworkofficial <mahbubworkofficial@gmail.com>
2025-05-05 19:39:22 +0600

    7:39

--- lib/res/assets/image_assets.dart
+++ lib/res/assets/image_assets.dart
@@ -3,28 +3,17 @@ class ImageAssets {
   //Icons
 
   static const String floting = 'assets/icons/floting.svg';
-  static const String menu = 'assets/icons/menu.svg';
   static const String notification = 'assets/icons/notification.svg';
-  static const String scan = 'assets/icons/scan.svg';
   static const String calender = 'assets/icons/calender.svg';
-  static const String achieve = 'assets/icons/calender.svg';
   static const String apple = 'assets/icons/apple.svg';
   static const String back_arrow = 'assets/icons/back_arrow.svg';
-  static const String create_logo = 'assets/icons/create_logo.svg';
-  static const String delete = 'assets/icons/delete.svg';
-  static const String edit = 'assets/icons/edit.svg';
   static const String email = 'assets/icons/email.svg';
   static const String facebook = 'assets/icons/facebook.svg';
   static const String google = 'assets/icons/google.svg';
-  static const String home = 'assets/icons/home.svg';
   static const String love = 'assets/icons/love.svg';
-  static const String obsecure = 'assets/icons/obsecure.svg';
-  static const String onboarding = 'assets/icons/onboarding.svg';
-  static const String password = 'assets/icons/password.svg';
   static const String post_report = 'assets/icons/post_report.svg';
   static const String send = 'assets/icons/send.svg';
   static const String share = 'assets/icons/share.svg';
-  // static const String splash = 'assets/icons/splash.svg';
   // static const String  = 'assets/icons/.svg';
 
 
